a638ec88b781cef93efa182fca8c1ee368cc7d21
Charles Shao <charles.shao@novabeyond.com>
2022-05-02 10:09:52 +0800

    AWS S3 Component practice demo

---
+++ src/main/java/com/example/awscomponent/AwsComponentApplication.java
@@ -0,0 +1,14 @@
+package com.example.awscomponent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsComponentApplication.class, args);
+		System.out.println("========== START ==========");
+	}
+
+}
